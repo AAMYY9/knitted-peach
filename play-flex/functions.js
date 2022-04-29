8c9378d3f3ece8035dc57eddc39d2738753f563b
AAMYY9 <gooneramy901@gmail.com>
2022-04-29 10:07:25 -0400

    4.29
    1

--- play-flex/functions.js
+++ play-flex/functions.js
@@ -12,11 +12,28 @@
 //   element[0].style["background-image"] = "url("+ images[x] + ") no-repeat;";
 // }
 // document.addEventListener("DOMContentLoaded", randomImage);
+
+// function randomImage(){
+//   var images = [
+//    'screen-grass.png',
+//    'win2.png',
+//    'win1.png'
+//    'win-death.jpeg'];
+//   var size = images.length;
+//   var x = Math.floor(size * Math.random());
+//   console.log(x);
+//   var element = document.getElementsByClassName('home-intro');
+//   console.log(element);
+//   element[0].style["background-image"] = "url("+ images[x] + ")";
+// }
+//
+// document.addEventListener("DOMContentLoaded", randomImage);
+
 function randomImage(){
   var images = [
    'screen-grass.png',
    'win2.png',
-   'win1.png'
+   'win1.png',
    'win-death.jpeg'];
   var size = images.length;
   var x = Math.floor(size * Math.random());
